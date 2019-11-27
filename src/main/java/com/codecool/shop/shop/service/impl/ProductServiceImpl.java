d559c1e341db95616124971c65343498fe836b93
Karol Karpeta <karol.karpeta@gmail.com>
2019-11-27 15:49:52 +0100

    product controller and template done, struggle to connect to remote db on elephantSQL (still not working)

--- src/main/java/com/codecool/shop/shop/service/impl/ProductServiceImpl.java
+++ src/main/java/com/codecool/shop/shop/service/impl/ProductServiceImpl.java
@@ -1,9 +1,16 @@
 package com.codecool.shop.shop.service.impl;
 
+import com.codecool.shop.shop.entity.Product;
 import com.codecool.shop.shop.repository.ProductRepository;
+import com.codecool.shop.shop.service.interfaces.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class ProductServiceImpl {
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
 
     private ProductRepository productRepository;
 
@@ -11,5 +18,31 @@ public class ProductServiceImpl {
     public ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
+
+    @Override
+    public List<Product> findAll() {
+        return this.productRepository.findAll();
+    }
+
+    @Override
+    public Product findById(int id) {
+        Optional<Product> product = this.productRepository.findById(id);
+        if(!product.isPresent()){
+            throw new RuntimeException("item with" + id + "not found");
+        }
+        return product.get();
+    }
+
+    @Override
+    public void save(Product product) {
+        this.productRepository.save(product);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        this.productRepository.deleteById(id);
+    }
+
+
 
 }
