ee82fd1325d6bb5c642a3be7fe910f3cc8be4a6c
ArekSzreiber <szreiber.arkadiusz@gmail.com>
2019-11-27 02:35:05 +0100

    initial commit

---
+++ src/main/java/com/codecool/shop/shop/CodecoolShopApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.shop.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecoolShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecoolShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/shop/shop/CodecoolShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.shop.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecoolShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
