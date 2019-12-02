43e0d5a4ff5c4f7dfd5872c7d9d73edbcc8a7983
ArekSzreiber <szreiber.arkadiusz@gmail.com>
2019-12-02 16:49:55 +0100

    Removal. Applied second convention for packages (for bigger projects - packages divided by topic, than function)

--- src/main/java/com/codecool/shop/products/ProductController.java
+++ src/main/java/com/codecool/shop/products/ProductController.java
@@ -1,12 +1,14 @@
-package com.codecool.shop.controllers;
+package com.codecool.shop.products;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+
+public class ProductController {
     @GetMapping("/")
-    public String showIndexPage() {
-        return "index";
+    public String showProductsList() {
+        return "products/products-list";
     }
+
 }
--- src/main/java/com/codecool/shop/shop/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.shop.shop.repository;
-
-import com.codecool.shop.shop.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
