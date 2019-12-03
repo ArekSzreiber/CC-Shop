9d1365f891060e338b71d0421ff5eb4ac2dfb67a
Karol Karpeta <karol.karpeta@gmail.com>
2019-12-03 15:42:04 +0100

    properties of product entity

--- src/main/java/com/codecool/shop/products/Product.java
+++ src/main/java/com/codecool/shop/products/Product.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.math.BigDecimal;
 
 @Data
 @Component
@@ -15,5 +17,30 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private int id;
+
+    @Column(name = "name")
+    @NotEmpty
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "image_url")
+    private String imageURL;
+
+    @Column(name = "properties")
+    private String properties;
+
+    @Column(name = "description")
+    private String description;
+
+/*
+    @ManyToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id", insertable = false, updatable = false)
+    private Category category;*/
+
+/*  @ManyToOne
+    @JoinColumn(name = "supplier_d", referencedColumnName = "supplier_id", insertable = false, updatable = false)
+    private Supplier supplier;*/
 
 }
