449a8ff19079fe37f5491c5e29be6021fb2b3ef9
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2019-12-09 11:58:34 +0100

    Categories add and update form almost merged

--- src/main/java/com/codecool/shop/category/CategoryController.java
+++ src/main/java/com/codecool/shop/category/CategoryController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -28,7 +31,7 @@ public class CategoryController {
     public String addCategoryForm(Model model) {
         Category category = new Category();
         model.addAttribute("category", category);
-        return "categories/categories-add";
+        return "categories/category-add";
     }
 
     @PostMapping("/categories/add")
