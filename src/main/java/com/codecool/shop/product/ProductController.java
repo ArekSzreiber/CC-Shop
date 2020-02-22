fb70dda2fd35bc463c5038cc4fd34223ce814f6c
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2020-02-22 13:00:01 +0100

    Sending all Products

---
+++ src/main/java/com/codecool/shop/product/ProductController.java
@@ -0,0 +1,23 @@
+package com.codecool.shop.product;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class ProductController {
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        return productService.getAllProducts();
+    }
+
+}
